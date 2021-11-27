99d5056baeaae4826bb1e752ed09ac2a76255132
avc0021 <avc0021@gmail.com>
2021-11-27 10:09:45 -0600

    start button functioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,26 @@
+const startButton = document.getElementById('start-btn')
+const quizTitle = document.getElementById('quiz-title')
+const questionContainerElement = document.getElementById('question-container')
 
+startButton.addEventListener('click',startGame)
 
 
 //Start quiz after click start button
+function startGame() {
+    startButton.classList.add('hide')
+    quizTitle.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+
+}
 
 
 //Questions
+function setNextQuestion(){
+
+}
+
+function selectAnswer(){
 
 
+}
 //Enter initals to save highscore
