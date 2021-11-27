6d0ad0b90f57b62004c32bd74f52095cf5c086f6
avc0021 <avc0021@gmail.com>
2021-11-27 10:32:12 -0600

    add random sort to questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,22 +5,42 @@ const questionContainerElement = document.getElementById('question-container')
 startButton.addEventListener('click',startGame)
 
 
-//Start quiz after click start button
+//Start quiz and remove title/start button
 function startGame() {
     startButton.classList.add('hide')
     quizTitle.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() -.5)
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
-
+    setNextQuestion()
 }
 
 
 //Questions
 function setNextQuestion(){
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function selectAnswer(){
 
 
 }
+
+const questions = [
+    {
+        question:'Conditional Statements must include which of the following?',
+        answer: [
+            { text: 'banana', correct: false },
+            { text: 'for', correct: false },
+            { text: 'if', correct: true },
+            { text: 'var', correct: false }
+
+        ]
+
+    }
+]
 //Enter initals to save highscore
